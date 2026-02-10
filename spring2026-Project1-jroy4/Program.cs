81f2fe90047f693cf0eef9f1607345b7b697f61b
jroy <jayyk0912@gmail.com>
2026-02-10 12:19:38 -0600

    Convert project to ASP.NET Web SDK

--- spring2026-Project1-jroy4/Program.cs
+++ spring2026-Project1-jroy4/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-Console.WriteLine("testing if this works");
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
